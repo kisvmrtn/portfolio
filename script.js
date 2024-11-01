eca595513da0cbfdde42e6fcde572db88b9f09d7
marton8411 <kisvaradi.marci@gmail.com>
2024-11-01 02:20:40 +0100

    themeSwitch kesz, iconok is frissulnek a valtoztataskor (if/else)

---
+++ script.js
@@ -0,0 +1,19 @@
+function themeSwitch() {
+  document.body.classList.toggle("light-theme");
+  const contactDark = document.querySelector(".contact_dark");
+  const contactLight = document.querySelector(".contact_light");
+  const imgDark = document.querySelector(".img_dark");
+  const imgLight = document.querySelector(".img_light");
+
+  if (document.body.classList.contains("light-theme")) {
+    contactDark.style.display = "none";
+    imgDark.style.display = "none";
+    contactLight.style.display = "grid";
+    imgLight.style.display = "grid";
+  } else {
+    contactDark.style.display = "grid";
+    imgDark.style.display = "grid";
+    contactLight.style.display = "none";
+    imgLight.style.display = "none";
+  }
+}
